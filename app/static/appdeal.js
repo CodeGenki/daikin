7cffd9a72ce460dd7d4d5d5d628ed14cd03244b1
EC2 Default User <ec2-user@ip-172-31-90-16.ec2.internal>
2018-07-06 14:30:58 +0000

    pw check stops from registering in cognito

--- app/static/appdeal.js
+++ app/static/appdeal.js
@@ -67,6 +67,7 @@ function registerDEAL(){
 	var phone_number = $('#registration_phone_number').val();
 	var address = $('#registration_address').val();	
 	var password = $('#registration_password').val();
+    var passwordC = $('#registrationC_password').val();
 
     var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolDataDEAL);
 
@@ -153,6 +154,9 @@ function registerDEAL(){
         document.getElementById("passwordError").innerHTML = "Please enter a password.";
         e = true;
     }
+    if (password != passwordC){
+        e = true;
+    } 
 
 
     if (!e){
@@ -166,8 +170,8 @@ function registerDEAL(){
                     var e4 = "Value at 'username' failed to satisfy constraint: Member must satisfy regular expression pattern: [\\p{L}\\p{M}\\p{S}\\p{N}\\p{P}]+";
                     var e5 = "Invalid phone number format.";
                     var e6 = "Invalid email address format.";
-                    if (err.message.search(e3) != -1 || err.message.search(e4) != -1){
-                        document.getElementById("usernameError").innerHTML = "Please enter a username with length greater than 1.";
+                    if (err.message.search(e3) != -1 || err.message.indexOf(e4) != -1){
+                        document.getElementById("usernameError").innerHTML = "Please enter a valid username with length greater than 1.";
                         e = true;      
                     }         
                     if (err.message.search(e1) != -1 || err.message.search(e2) != -1){
